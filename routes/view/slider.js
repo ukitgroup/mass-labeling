923fdac5d15ce53f5ef1046021bb5ced6ff8029c
Vladislav Yakovlev <vlad309523@gmail.com>
2018-03-23 10:03:39 +0300

    refactor: remove slider stub

--- routes/view/slider.js
+++ routes/view/slider.js
@@ -4,15 +4,11 @@ const Slider = require('../../model/slider');
 const router = require('express').Router();
 
 
-router.get('/', async (req, res) => {
-	res.render('slider/stub');
-});
-
 router.get('/:name', async (req, res, next) => {
 	try {
 		const items = await Slider.getAllByName(req.params.name);
 
-		res.render('slider/slider', {
+		res.render('slider', {
 			layout: false,
 			items: await Slider.serializeAll(items),
 		});
