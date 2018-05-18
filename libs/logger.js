36e5ba872dc6d671d4ca9d13d99a3cc294423f52
Vladislav Yakovlev <vlad309523@gmail.com>
2018-05-18 12:11:29 +0300

    feat(logger): pretty print

--- libs/logger.js
+++ libs/logger.js
@@ -1,11 +1,26 @@
 const bunyan = require('bunyan');
+const { spawn } = require('child_process');
+const through = require('through');
 
-const config = require('../config');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+
+function createPrettyStream() {
+	const stream = through();
+
+	const formatter = spawn('npx', ['bunyan'], {
+		stdio: [null, process.stdout, process.stderr],
+	});
+	stream.pipe(formatter.stdin);
+
+	return stream;
+}
 
 
 module.exports = bunyan.createLogger({
-	name: config.get('logger.name'),
-	stream: process.stdout,
-	level: config.get('logger.level'),
+	name: 'app',
+	stream: process.stdout.isTTY ? createPrettyStream() : process.stdout,
+	level: isDevelopment ? 'debug' : 'info',
 	serializers: bunyan.stdSerializers,
 });
